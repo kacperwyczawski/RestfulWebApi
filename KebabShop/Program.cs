2f9ae0b5dd844283eed129359e72d8bfb648af3a
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-14 09:36:10 +0200

    Add HTTP Get methods

--- KebabShop/Program.cs
+++ KebabShop/Program.cs
@@ -7,6 +7,15 @@ builder.Services.AddDbContext<KebabDatabaseContext>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/kebabs", async (KebabDatabaseContext db) =>
+    await db.Kebabs.ToListAsync());
+
+app.MapGet("/kebabs/{id}", async (int id, KebabDatabaseContext db) =>
+    await db.Kebabs.FindAsync(id) is { } kebab // check for null
+        ? Results.Ok(kebab) // 200 response
+        : Results.NotFound()); // 404 response
+
+app.MapGet("/kebabs/vege", async (KebabDatabaseContext db) =>
+    await db.Kebabs.Where(kebab => kebab.IsVege).ToListAsync());
 
 app.Run();
