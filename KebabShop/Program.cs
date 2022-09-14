e35280a5a93ecc79f5dd6733c08e3212b3f4cb6e
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-14 09:35:21 +0200

    Inject database context to app

--- KebabShop/Program.cs
+++ KebabShop/Program.cs
@@ -1,4 +1,10 @@
+using KebabShop;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<KebabDatabaseContext>
+    (options => options.UseInMemoryDatabase("KebabDatabase"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
