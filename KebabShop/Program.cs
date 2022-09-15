54d0a532745cd918d9d45371d54b14da3774a396
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-09-15 19:36:58 +0200

    Add HTTP Post and Put methods

--- KebabShop/Program.cs
+++ KebabShop/Program.cs
@@ -18,4 +18,28 @@ app.MapGet("/kebabs/{id}", async (int id, KebabDatabaseContext db) =>
 app.MapGet("/kebabs/vege", async (KebabDatabaseContext db) =>
     await db.Kebabs.Where(kebab => kebab.IsVege).ToListAsync());
 
+app.MapPost("/kebabs", async (Kebab kebab, KebabDatabaseContext db) =>
+{
+    db.Kebabs.Add(kebab);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/kebabs/{kebab.Id}", kebab);
+});
+
+app.MapPut("/kebabs/{id}", async (int id, Kebab inputKebab, KebabDatabaseContext db) =>
+{
+    var kebabToUpdate = await db.Kebabs.FindAsync(id);
+
+    if (kebabToUpdate is null)
+        return Results.NotFound();
+    
+    // ReSharper disable once RedundantAssignment
+    kebabToUpdate = kebabToUpdate with
+        { Name = inputKebab.Name, IsVege = inputKebab.IsVege, Price = inputKebab.Price };
+    
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 app.Run();
